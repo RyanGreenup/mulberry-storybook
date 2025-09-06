25152b41c0681b4d69cdd971571084370198e0ad
Ryan G <exogenesis@protonmail.com>
2025-09-06 18:22:30 +1000

    Add panda button story

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,34 +1,19 @@
-import { mergeProps, splitProps } from 'solid-js';
-import './button.css';
+import { JSXElement } from "solid-js";
+import { css } from "../styled-system/css";
 
-export interface ButtonProps {
-    primary?: boolean;
-    backgroundColor?: string;
-    size?: 'small' | 'medium' | 'large';
-    label: string;
-    [key: string]: any;
-}
-
-/** Primary UI component for user interaction */
-export const Button = (_props: ButtonProps) => {
-    const [props, rest] = splitProps(
-        mergeProps({ primary: false, backgroundColor: null, size: 'medium' }, _props),
-        ['primary', 'backgroundColor', 'size', 'label']
-    );
-    const mode = () => props.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-
-    return (
-        <button
-            type="button"
-            class={ [
-                'storybook-button',
-                `storybook-button--${ props.size }`,
-                mode(),
-            ].join(' ') }
-            style={ props.backgroundColor ? { 'background-color': props.backgroundColor } : undefined }
-            { ...rest }
-        >
-            {props.label}
-        </button>
-    );
+export const Button = (props: { children: JSXElement }) => {
+  return (
+    <button
+      class={css({
+        bg: "red.300",
+        fontFamily: "Inter",
+        px: "4",
+        py: "3",
+        borderRadius: "md",
+        _hover: { bg: "red.400" },
+      })}
+    >
+      {props.children}
+    </button>
+  );
 };
